6cddd4cb24e2dbb38da020bd39340d4bb484992a
Vinicius Verona <vinicius.verona@aluno.ufop.edu.br>
2023-04-25 20:45:11 +0200

    Add collision/multicollision

---
+++ impl.c
@@ -0,0 +1,332 @@
+/*************************************************************
+    AUTHOR  : Vinicius Gabriel Angelozzi Verona de Resende
+    MAIL    : verona.projects@tutanota.com        / 
+              angelozv@etu.univ-grenoble-alpes.fr / 
+              vinicius.verona@aluno.ufop.edu.br
+    YEAR    : 2022
+    COURSE  : Introduction to Cryptology
+    PROFESSOR: Bruno Grenet
+*************************************************************/
+
+#include "impl.h"
+
+int SAMPLES = 0;
+
+/********************************************************************************/
+/********************************************************************************/
+/********************************************************************************/
+/****************************** AUXILIAR FUNCTIONS ******************************/
+/********************************************************************************/
+/********************************************************************************/
+/********************************************************************************/
+
+void print_uint8(const uint8_t* x, const size_t len, const char* s) {
+    for (int i = 0; i < len; i++) {
+        printf("%d ", x[i]);
+    }
+
+    printf("%s", s);
+}
+
+// function to generate a random message
+void random_message(uint8_t* m, uint8_t size) {
+    for (uint8_t i = 0; i < size; i++) {
+        m[i] = (uint8_t)xoshiro256starstar_random();
+    }
+}
+
+int cmp_hashes(uint8_t* h1, uint8_t* h2, uint8_t size) {
+    for (int i = 0; i < size; i++) {
+        if (h1[i] != h2[i]) return 1;
+    }
+
+    return 0;
+}
+
+void copy_hash(uint8_t* dst, uint8_t* src, uint8_t size) {
+    for (int i = 0; i < size; i++) {
+        dst[i] = src[i];
+    }
+}
+
+void copy_msg(uint8_t* dst, uint8_t* src, uint8_t size) {
+    for (int i = 0; i < size; i++) {
+        dst[i] = src[i];
+    }
+}
+
+HASH_TABLE* create_hash_table(int size) {
+    HASH_TABLE* hash_table = (HASH_TABLE*)malloc(size * sizeof(HASH_TABLE));
+
+    for (int i = 0; i < size; i++) {
+        hash_table[i].list = NULL;
+    }
+
+    return hash_table;
+}
+
+void free_hash_table(HASH_TABLE* hash_table, int size) {
+    for (int i = 0; i < size; i++) {
+        if (hash_table[i].list != NULL) {
+            free_list(hash_table[i].list);
+        }
+    }
+
+    free(hash_table);
+}
+
+void free_list(HASH_TABLE_LIST* list) {
+    HASH_TABLE_CELL* aux = list->fst_cell;
+
+    while (aux != NULL) {
+        HASH_TABLE_CELL* aux2 = aux->next;
+        free(aux);
+        aux = aux2;
+    }
+
+    free(list);
+}
+
+/**
+ * Given a uint8_t array, return a string representation of the array
+*/
+void get_string_hex(uint8_t* h, char* hash_string, int size) {
+    // There is an error here
+    for (int i = 0; i < size; i++) {
+        sprintf(hash_string + i, "%02X", h[i]);
+    }
+}
+
+int lookup_hash(HASH_TABLE* hash_table, uint8_t* h, uint8_t* m, int key) {
+    HASH_TABLE_CELL* aux;
+
+    if (hash_table[key].list == NULL) return 0;
+
+    aux = hash_table[key].list->fst_cell;
+
+    // While the end of the list is not reached or the element is not found, change aux to the next element
+    while (aux != NULL && (cmp_hashes(aux->item->hash, h, 6) != 0 || cmp_hashes(aux->item->message, m, BLEN) == 0)) {
+        aux = aux->next;
+    }
+
+    // If the element was found, return 1
+    if (aux != NULL) {
+        #ifdef VERBOSE
+
+            char message_string1[2*BLEN]; get_string_hex(aux->item->message, message_string1, BLEN);
+            char message_string2[2*BLEN]; get_string_hex(m, message_string2, BLEN);
+
+            printf("-: %s %s %lf\n", message_string1, message_string2, log2(SAMPLES));
+
+        #endif
+        return 1;
+    }
+
+    return 0;
+}
+
+void add_to_hash_list(HASH_TABLE* hash_table, uint8_t* h, uint8_t* m, long int key) {
+    HASH_TABLE_CELL* new_cell = (HASH_TABLE_CELL*)malloc(sizeof(HASH_TABLE_CELL));
+    HASH_TABLE_ITEM* new_item = (HASH_TABLE_ITEM*)malloc(sizeof(HASH_TABLE_ITEM));
+
+    copy_hash(new_item->hash, h, 6);
+    copy_msg(new_item->message, m, BLEN);
+
+    new_cell->item = new_item;
+    new_cell->next = NULL;
+
+    if (hash_table[key].list == NULL) {
+        hash_table[key].list = (HASH_TABLE_LIST*)malloc(sizeof(HASH_TABLE_LIST));
+        hash_table[key].list->fst_cell = new_cell;
+        hash_table[key].list->size = 0;
+    } else {
+        hash_table[key].list->lst_cell->next = new_cell;
+    }
+
+    hash_table[key].list->lst_cell = new_cell;
+    hash_table[key].list->size += 1;
+}
+
+uint64_t get_hash_key(uint8_t* hash) {
+    uint64_t key = 1;
+    for (int i = 0; i < 6; i++) {
+        key = key * 13 + hash[i];
+    }
+
+    return key % HT_SIZE;
+}
+
+int add_hash(HASH_TABLE* hash_table, uint8_t* h, uint8_t* m, long int key) {
+    if (lookup_hash(hash_table, h, m, key) != 1) {
+        add_to_hash_list(hash_table, h, m, key);
+        return 1;
+    } else {
+        return -1;
+    }
+    return 0;
+}
+
+uint8_t* get_message(HASH_TABLE* hash_table, long int key, uint8_t* h) {
+    HASH_TABLE_CELL* aux;
+
+    if (hash_table[key].list == NULL) return NULL;
+
+    aux = hash_table[key].list->fst_cell;
+
+    // While the end of the list is not reached or the element is not found, change aux to the next element
+    while (aux != NULL && cmp_hashes(aux->item->hash, h, 6) != 0) {
+        aux = aux->next;
+    }
+
+    // If the element was found, return 1
+    if (aux != NULL) {
+        return aux->item->message;
+    }
+
+    return NULL;
+}
+
+
+/********************************************************************************/
+/********************************************************************************/
+/********************************************************************************/
+/************************************ TASKS *************************************/
+/********************************************************************************/
+/********************************************************************************/
+/********************************************************************************/
+
+
+void iterated_tcz48_dm(const uint8_t* m, const size_t len, uint8_t h[6]) {
+    uint64_t isMultiple = len % BLEN == 0;
+
+    if (!isMultiple) return;
+
+    // simple IV
+    h[0] = IVB0;
+    h[1] = IVB1;
+    h[2] = IVB2;
+    h[3] = IVB3;
+    h[4] = IVB4;
+    h[5] = IVB5;
+
+    for (uint64_t b = 0; b < len; b += BLEN) {
+        tcz48_dm(m + b, h);
+    }
+}
+
+void print(const uint8_t* x, const size_t len, const char* s) {
+    for (int i = 0; i < len; i++) {
+        printf("%02X", x[i]);
+    }
+
+    printf("%s", s);
+}
+
+
+void collision(uint8_t h[6], uint8_t* m1, uint8_t* m2) {
+    // Hash table used to store all hashes generated and its messages
+    HASH_TABLE* hash_table = create_hash_table(HT_SIZE);
+    uint8_t null_hash[6] = {0};
+
+    int count = 0;
+
+    while (1) {
+        count++;
+        SAMPLES++;
+        uint8_t h1[6], h2[6];
+
+        // Generate a random message and compute its hash
+        random_message(m2, BLEN);
+        iterated_tcz48_dm(m2, BLEN, h2);
+
+        // Check if the hash is already in the hash table
+        uint64_t hidx = get_hash_key(h2);
+
+        #ifdef DEBUG
+            if ((count - 1) % 1000000 == 0) {
+                printf("Count: %d\n", (count - 1) / 1000000);
+            }
+        #endif
+
+        if (hash_table[hidx].list != NULL) {
+            long int idx = lookup_hash(hash_table, h2, m2, hidx);
+
+            if (idx) {
+                // Collision found, copy hash to h and return
+                copy_hash(h, h2, 6);
+                copy_msg(m1, get_message(hash_table, hidx, h2), BLEN);
+                break;
+
+            } else {
+                add_hash(hash_table, h2, m2, hidx);
+            }
+
+        } else {
+            // Add the hash to the hash table
+            add_hash(hash_table, h2, m2, hidx);
+        }
+    }
+
+    // Free hash_table memory
+    free_hash_table(hash_table, HT_SIZE);
+}
+
+double multicollision(int t) {
+    uint8_t h[6], m1[BLEN], m2[BLEN];
+    uint8_t pairs[2*t][BLEN], hashes[2*t][6];
+
+    if (SAMPLES != 0) SAMPLES = 0;
+
+    for (int i = 0; i < t; i++) {
+        collision(h, m1, m2);
+
+        // Store m1 and m2 so that they can be used to create future messages with the same hash
+        copy_msg(pairs[i], m1, BLEN);
+        copy_msg(pairs[i+t], m2, BLEN);
+        copy_hash(hashes[i], h, 6);
+    }
+
+    // Print each of the t pair of messages with the same hash
+    #ifdef VERBOSE
+        for (int i = 0; i < t; i++) {
+            char message_string1[2*BLEN]; get_string_hex(pairs[i], message_string1, BLEN);
+            char message_string2[2*BLEN]; get_string_hex(pairs[i+t], message_string2, BLEN);
+
+            printf("%s %s\n", message_string1, message_string2);
+        }
+    #endif
+
+    // Print all possible combination of pairs of messages with the same hash, in a way that each pair is only printed once
+    for (int i = 0; i < t; i++) {
+        for (int j = i+1; j < t; j++) {
+            char message_string1[2*BLEN]; get_string_hex(pairs[i], message_string1, BLEN);
+            char message_string2[2*BLEN]; get_string_hex(pairs[i+t], message_string2, BLEN);
+            char message_string3[2*BLEN]; get_string_hex(pairs[j], message_string3, BLEN);
+            char message_string4[2*BLEN]; get_string_hex(pairs[j+t], message_string4, BLEN);
+
+            printf("H(%s || %s) == H(%s || %s) => ", message_string1, message_string2, message_string3, message_string4);
+            print(h, 6, "\n");
+        }
+    }
+
+
+    #ifdef DEBUG_MESSAGE
+        // Print each of the t pair of messages in uint8_t format
+        for (int i = 0; i < t; i++) {
+            printf("m1: ");
+            print_uint8(pairs[i], BLEN, " || ");
+            printf("m2: ");
+            print_uint8(pairs[i+t], BLEN, "\n");
+        }
+    #endif
+
+    return SAMPLES;
+}
+
+double unbalanced_collision(uint8_t h[6], uint8_t m1[16], uint8_t* m2, const size_t len) {
+    // TODO: Implement this function
+}
+
+double expandable_message(int t) {
+    // TODO: Implement this function
+}
---
+++ impl.h
@@ -0,0 +1,140 @@
+/*************************************************************
+    AUTHOR  : Vinicius Gabriel Angelozzi Verona de Resende
+    MAIL    : verona.projects@tutanota.com        / 
+              angelozv@etu.univ-grenoble-alpes.fr / 
+              vinicius.verona@aluno.ufop.edu.br
+    YEAR    : 2022
+    COURSE  : Introduction to Cryptology
+    PROFESSOR: Bruno Grenet
+*************************************************************/
+
+#ifndef IMPL_H
+#define IMPL_H
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "mc48.h"
+#include "xoshiro256starstar.h"
+
+#define DEBUG_MESSAGE                                     // Uncomment to print the messages in uint8_t format in multicollision
+
+// #define VERBOSE                                           // Uncomment to print the collision
+#define BLEN 16                                           // Block Length in Bytes (16 bytes = 128 bits)
+#define BLEN_BITS 128                                     // Block Length in Bits
+#define HASH_SIZE 48                                      // Hash size in bits
+#define HT_SIZE (int)ceil(sqrt(pow(2, (HASH_SIZE - 1))))  // Size of the hash table
+
+// Hash table struct
+typedef struct {
+    uint8_t hash[6];
+    uint8_t message[BLEN];
+} HASH_TABLE_ITEM;
+
+typedef struct CELL {
+    HASH_TABLE_ITEM* item;
+    struct CELL* next;
+} HASH_TABLE_CELL;
+
+typedef struct {
+    HASH_TABLE_CELL *fst_cell, *lst_cell;
+    int size;
+} HASH_TABLE_LIST;
+
+typedef struct {
+    HASH_TABLE_LIST* list;
+} HASH_TABLE;
+
+/**
+ * Given a hash, return the key of the hash table
+ * In this case the index of the hash table is calculated as follows:
+ * The key starts with a value 1, and each byte of the hash is multiplied by the prime 13 and added to the previou value of the key.
+ * In the end, the key is modded by the size of the hash table
+ */
+uint64_t get_hash_key(uint8_t* hash);
+
+/**
+ * Compare two byte arrays
+ * @param h1 the first byte array
+ * @param h2 the second byte array
+ * @param size the byte-length of both arrays
+ * Output: 0 if the arrays are equal, 1 otherwise
+ */
+int cmp_hashes(uint8_t* h1, uint8_t* h2, uint8_t size);
+void copy_hash(uint8_t* dst, uint8_t* src, uint8_t size);
+
+/**
+ * Add an element to the hash table
+ */
+int add_hash(HASH_TABLE* hash_table, uint8_t* h, uint8_t* m, long int idx);
+
+/**
+ * Given a hash_table look if h is found in hash_table[key]
+ * If it is found, return 1
+ */
+int lookup_hash(HASH_TABLE* hash_table, uint8_t* h, uint8_t* m, int key);
+
+/**
+ * Add a hash to the list in case of index collision
+ */
+void add_to_hash_list(HASH_TABLE* hash_table, uint8_t* h, uint8_t* m, long int key);
+void free_list(HASH_TABLE_LIST* list);
+void free_hash_table(HASH_TABLE* hash_table, int size);
+HASH_TABLE* create_hash_table(int size);
+
+/**
+ * Creates a function get_message that given a hash and a key, returns the message associated with that hash
+*/
+uint8_t* get_message(HASH_TABLE* hash_table, long int key, uint8_t* h);
+
+/*****************************************************************/
+/***************************** Tasks *****************************/
+/*****************************************************************/
+
+/**
+ * Given a message, compress the message using the Davies-Meyer compression function
+ * @param m the message to be compressed, whose byte-length is required to be a multiple of 'w'
+ * @param len the byte-length of 'm'
+ * @param h variable in which the result of the compression is written to.
+ */
+void iterated_tcz48_dm(const uint8_t* m, const size_t len, uint8_t h[6]);
+
+/**
+ * Print a byte array in hexadecimal format followed by S
+ * @param x the byte array to be printed
+ * @param len the byte-length of 'x'
+ * @param s the string to be printed after the byte array
+ */
+void print(const uint8_t* x, const size_t len, const char* s);
+
+/**
+ * 1 - Create a hash table
+ * 2 - Generate a random message m2 and compute its hash h2
+ * 3 - Check if h2 is in the hash table
+ * 4 - If h2 is in the hash table, then we found a collision
+ * 5 - If h2 is not in the hash table, then add it with the message to the hash table and go to step 2
+ */
+void collision(uint8_t h[6], uint8_t* m1, uint8_t* m2);
+
+/**
+ * Find a multicollision 2^t executing collision t times 
+*/
+double multicollision(int t);
+
+/**
+ * Finds a collision of the form f(h, m1) = F(h, m2) where m2 has len blocks
+ * @param h array to store the hash of the collision
+ * @param m1 array to store the first message of the collision
+ * @param m2 array to store the second message of the collision
+*/
+double unbalanced_collision(uint8_t h[6], uint8_t m1[16], uint8_t* m2, const size_t len);
+
+/**
+ * Computes an expandable message for F of 2^t messages of length t to t + 2^t − 1
+*/
+double expandable_message(int t);
+
+#endif
